72ddae203594dbb01358299577d19402e37d5496
Tiffany Huang <tiff8433@users.noreply.github.com>
2015-12-24 20:16:51 -0800

    Update request-handler.js

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,34 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var urlPath = req.url === '/' ? '/index.html' : req.url;
+  if (req.method === 'GET'){
+    helpers.serveAssets(res, urlPath);
+  };
+  if (req.method === 'POST') {
+    helpers.collectData(req, function(data){
+
+      var urlPath = data.split('=')[1];
+      console.log('urlPath', urlPath);
+      
+      archive.isUrlInList(urlPath, function(found){
+        if (found){
+          archive.isUrlArchived(urlPath, function(found){
+            if(found) {
+              helpers.sendRedirect(res, '/' + urlPath);
+            } else {
+              helpers.sendRedirect(res, '/loading.html');
+            }
+          });
+        } else {
+          archive.addUrlToList(urlPath, function(){
+            helpers.sendRedirect(res, '/loading.html');
+          });
+        }
+      });
+    });
+  }
 };
